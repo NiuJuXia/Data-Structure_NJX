3f291e9638a47dd9f76e97b6a80a6f26d127a5ef
song mengx <niujuxiah@gmail.com>
2020-10-25 15:07:00 +0800

    测试提交1

---
+++ 数据结构c语言实现/数据结构c语言实现/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  数据结构c语言实现
+//
+//  Created by 宋宋梦醒 on 2020/10/25.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, Data-Structure!\n");
+    return 0;
+}
